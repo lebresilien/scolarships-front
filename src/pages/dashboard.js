5dba6365be2650306f10241ab1634cd32da63481
Tapondjou Petito <petitotapondjou@gmail.com>
2023-12-07 19:56:14 +0100

    fix charts error

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -88,8 +88,8 @@ const Dashboard = (props) => {
                                         </div>
 
                                         <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-                                            <ChartOne months={data?.transactions?.months} values={data?.transactions?.values} />
-                                            <ChartTwo days={data?.transactions?.days} values={data?.transactions?.day_values}/> 
+                                            {data?.transactions && <ChartOne months={data?.transactions?.months} values={data?.transactions?.values} />}
+                                            {data?.transactions && <ChartTwo days={data?.transactions?.days} values={data?.transactions?.day_values}/>} 
                                         </div>
 
                                     </div>:
