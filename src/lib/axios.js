deac27af281555599fe006262c5065c60a38201f
Tapondjou Petito <petitotapondjou@gmail.com>
2023-12-20 17:54:38 +0100

    axios set header

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-XSRF-TOKEN': cookie.get('XSRF-TOKEN')
     },
     withCredentials: true,
 })
