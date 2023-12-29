eae70abbb6afacb48c3b07c23a1e33c9c71ae489
Tapondjou Petito <petitotapondjou@gmail.com>
2023-12-29 14:24:05 +0100

    remove cookies

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,6 @@
 import Axios from 'axios'
-import Cookies from 'js-cookie'
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN')
-    },
     withCredentials: true,
 })
 
